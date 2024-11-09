08b21c0fc55fd48f2501e6f4a94d7b92c604ee2f
Jahtofunmi Osho <oshotofunmi@gmail.com>
2024-11-09 23:39:01 +0100

    implement restaurant fetch and update

--- src/dtos/restaurant.dto.ts
+++ src/dtos/restaurant.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class CreateRestaurantDto {
   @IsString()
@@ -10,4 +10,14 @@ export class CreateRestaurantDto {
   @IsString()
   @IsEmail()
   email!: string;
+}
+
+export class UpdateRestaurantDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
 }
--- src/routes/restaurant.routes.ts
+++ src/routes/restaurant.routes.ts
@@ -1,16 +1,37 @@
 import express from 'express';
 import { validateData } from '../middleware/validation.middleware';
-import { CreateRestaurantDto } from '../dtos/restaurant.dto';
-import { registerResturant } from '../controllers/restaurantController';
+import {
+  CreateRestaurantDto,
+  UpdateRestaurantDto,
+} from '../dtos/restaurant.dto';
+import {
+  getRestaurants,
+  getSingleRestaurant,
+  registerResturant,
+  updateRestaurant,
+} from '../controllers/restaurant.controller';
 import { protect } from '../controllers/authController';
 
 const router = express.Router();
 
+// get routes
+router.get('/', getRestaurants);
+router.get('/:id', getSingleRestaurant);
+
+// post routes
 router.post(
   '/register',
   protect,
   validateData(CreateRestaurantDto),
   registerResturant,
+);
+
+// patch routes
+router.patch(
+  '/:id',
+  protect,
+  validateData(UpdateRestaurantDto),
+  updateRestaurant,
 );
 
 export default router;
